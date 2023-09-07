8fb9ec2db5a8c88c53351ca6834342f05f86540b
Mario Fernández <mariofernandez@MacBook-Pro-de-Mario.local>
2023-09-07 19:43:45 +0200

    Commit inicial

---
+++ src/test/java/inditex/productpriceservice/ProductPriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package inditex.productpriceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPriceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
