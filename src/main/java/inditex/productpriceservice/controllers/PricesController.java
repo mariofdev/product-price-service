5072b71fba510c5f669d25ef6059951dab08a9df
Mario Fernández <mariofernandez@MacBook-Pro-de-Mario.local>
2023-09-10 22:27:54 +0200

    0.9: Se añade la lógica de filtrado

--- src/main/java/inditex/productpriceservice/controllers/PricesController.java
+++ src/main/java/inditex/productpriceservice/controllers/PricesController.java
@@ -1,9 +1,6 @@
 package inditex.productpriceservice.controllers;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
